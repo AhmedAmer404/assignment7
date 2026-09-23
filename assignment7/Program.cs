8cb9933cc9ccc45ae88dcca9cb0dc5e6c506faa2
Ahmed Amer <ahmedamerxx5@gmail.com>
2026-09-23 15:35:47 +0300

    question 4

--- assignment7/Program.cs
+++ assignment7/Program.cs
@@ -83,6 +83,7 @@ namespace Assignment7
 
     //  #endregion
 
+
     #region Question 2
     public class Shipment
     {
@@ -339,6 +340,118 @@ namespace Assignment7
 
 
     }
+
+    #endregion
+
+    #region Question 4
+    public class DeliveryCenter
+    {
+        private Shipment[] shipments;
+
+        public string CenterName { get; set; }
+
+
+        public DeliveryCenter(string centerName)
+        {
+            CenterName = centerName;
+            shipments = new Shipment[20];
+        }
+
+
+        
+        public Shipment this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < shipments.Length)
+                    return shipments[index];
+
+                return null;
+            }
+
+            set
+            {
+                if (index >= 0 && index < shipments.Length)
+                    shipments[index] = value;
+            }
+        }
+
+
+        
+        public Shipment this[string trackingCode]
+        {
+            get
+            {
+                for (int i = 0; i < shipments.Length; i++)
+                {
+                    if (shipments[i] != null &&
+                        shipments[i].TrackingCode == trackingCode)
+                    {
+                        return shipments[i];
+                    }
+                }
+
+                return null;
+            }
+        }
+
+
+       
+        public bool AddShipment(Shipment shipment)
+        {
+            for (int i = 0; i < shipments.Length; i++)
+            {
+                if (shipments[i] == null)
+                {
+                    shipments[i] = shipment;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        
+        public bool RemoveShipment(string trackingCode)
+        {
+            for (int i = 0; i < shipments.Length; i++)
+            {
+                if (shipments[i] != null &&
+                    shipments[i].TrackingCode == trackingCode)
+                {
+                    shipments[i] = null;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        
+        public void PrintAllShipments()
+        {
+            Console.WriteLine($"\n===== {CenterName} =====");
+
+            bool found = false;
+
+            for (int i = 0; i < shipments.Length; i++)
+            {
+                if (shipments[i] != null)
+                {
+                    shipments[i].PrintShipment();
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("No shipments available.");
+            }
+        }
+    }
+
 
     #endregion
 
