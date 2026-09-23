081c3299b9cbfbcda94e8727b6de6d4f98c7c708
Ahmed Amer <ahmedamerxx5@gmail.com>
2026-09-23 14:07:49 +0300

    Question2

--- assignment7/Program.cs
+++ assignment7/Program.cs
@@ -23,110 +23,160 @@ namespace Assignment7
 
     }
 
-    public struct Shipment
+
+    ////  #region Question 1
+    //  struct DeliveryCenter
+    //  { 
+    //      private Shipment[] shipments;
+
+    //      public DeliveryCenter(int capacity = 10)
+    //      {
+    //          shipments = new Shipment[capacity];
+    //      }
+    //      public Shipment this[int index]
+    //      {
+    //          get
+    //          {
+    //              if (index >= 0 && index < shipments.Length)
+    //              {
+    //                  return shipments[index];
+    //              }
+    //              return default;
+    //          }
+    //          set
+    //          {
+    //              if(index >= 0 && index < shipments.Length)
+    //              {
+    //                  shipments[index] = value;
+    //              }
+    //          }
+    //      }
+    //      public Shipment this[string index]
+    //      {
+    //          get
+    //          {
+    //              foreach (var shipment in shipments)
+    //              {
+    //                  if (!string.IsNullOrWhiteSpace(shipment.TrackingCode) && shipment.TrackingCode == index)
+    //                  {
+    //                      return shipment;
+    //                  }
+    //              }
+    //              return default;
+    //          }
+    //      }
+    //      public  bool AddShipment(Shipment shipment)
+    //      {
+    //          for (int i = 0; i < shipments.Length; i++)
+    //          {
+    //              if (string.IsNullOrWhiteSpace(shipments[i].TrackingCode))
+    //              {
+    //                  shipments[i] = shipment;
+    //                  return true;
+    //              }
+    //          }
+
+    //          return false;
+    //      }
+
+    //  }
+
+    //  #endregion
+
+    #region Question 2
+    public class Shipment
     {
         private string trackingCode;
         private string description;
-
-        private double weight;
-
+        private decimal weight;
         private decimal deliveryFee;
-
-        public DeliveryAddress Destination
-        {
-            get;
-            set;
-        }
 
         public string TrackingCode
         {
-            get
-            {
-                return trackingCode;
-            }
+            get { return trackingCode; }
             private set
             {
                 if (!string.IsNullOrWhiteSpace(value))
-                {
                     trackingCode = value;
-                }
-
             }
         }
+
         public string Description
         {
-            get
-            {
-                return description;
-
-            }
+            get { return description; }
             set
             {
                 if (!string.IsNullOrWhiteSpace(value))
-                {
                     description = value;
-                }
-
             }
-
         }
-        public double Weight
+
+        public decimal Weight
         {
-            get
-            {
-                return weight;
-            }
+            get { return weight; }
             set
             {
                 if (value > 0)
-                {
                     weight = value;
-                }
             }
         }
+
         public decimal DeliveryFee
         {
-            get
-            {
-                return deliveryFee;
-            }
+            get { return deliveryFee; }
             private set
             {
                 if (value > 0)
-                {
                     deliveryFee = value;
-                }
             }
         }
-        public double EstimatedCost
+
+        public DeliveryAddress Destination { get; set; }
+
+
+        
+        public decimal EstimatedCost
         {
             get
             {
-                return 5 * Weight + (double)DeliveryFee;
+                return DeliveryFee + (Weight * 5);
             }
         }
+
+
+        
         public Shipment(string trackingCode)
         {
-            this.trackingCode = "Unknown";
-            this.description = "Unknown";
-            this.weight = 1;
-            this.deliveryFee = 50;
-            Destination = new DeliveryAddress();
-        }
-        public Shipment(string trackingCode, string description, double weight, decimal deliveryFee, DeliveryAddress destination)
-        {
-            this.trackingCode = "Unknown";
-            this.description = "Unknown";
-            this.weight = 1;
-            this.deliveryFee = 50;
-            Destination = destination;
+            TrackingCode = trackingCode;
 
+            Description = "Unknown";
+            Weight = 1;
+            DeliveryFee = 50;
+
+            Destination = new DeliveryAddress(
+                "Unknown",
+                "Unknown",
+                0
+            );
+        }
+
+
+        
+        public Shipment(
+            string trackingCode,
+            string description,
+            decimal weight,
+            decimal deliveryFee,
+            DeliveryAddress destination)
+        {
             TrackingCode = trackingCode;
             Description = description;
             Weight = weight;
             DeliveryFee = deliveryFee;
             Destination = destination;
         }
+
+
         public void UpdateDeliveryFee(decimal newFee)
         {
             if (newFee > 0)
@@ -134,74 +184,22 @@ namespace Assignment7
                 DeliveryFee = newFee;
             }
         }
-        public void PrintShipment()
-        {
 
+
+        public virtual void PrintShipment()
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Type          : {GetType().Name}");
             Console.WriteLine($"Tracking Code : {TrackingCode}");
             Console.WriteLine($"Description   : {Description}");
             Console.WriteLine($"Weight        : {Weight}");
             Console.WriteLine($"Delivery Fee  : {DeliveryFee}");
             Console.WriteLine($"Destination   : {Destination.GetFullAddress()}");
             Console.WriteLine($"Estimated Cost: {EstimatedCost}");
-
+            Console.WriteLine("--------------------------------");
         }
     }
-    #region Question 1
-    struct DeliveryCenter
-    { 
-        private Shipment[] shipments;
 
-        public DeliveryCenter(int capacity = 10)
-        {
-            shipments = new Shipment[capacity];
-        }
-        public Shipment this[int index]
-        {
-            get
-            {
-                if (index >= 0 && index < shipments.Length)
-                {
-                    return shipments[index];
-                }
-                return default;
-            }
-            set
-            {
-                if(index >= 0 && index < shipments.Length)
-                {
-                    shipments[index] = value;
-                }
-            }
-        }
-        public Shipment this[string index]
-        {
-            get
-            {
-                foreach (var shipment in shipments)
-                {
-                    if (!string.IsNullOrWhiteSpace(shipment.TrackingCode) && shipment.TrackingCode == index)
-                    {
-                        return shipment;
-                    }
-                }
-                return default;
-            }
-        }
-        public  bool AddShipment(Shipment shipment)
-        {
-            for (int i = 0; i < shipments.Length; i++)
-            {
-                if (string.IsNullOrWhiteSpace(shipments[i].TrackingCode))
-                {
-                    shipments[i] = shipment;
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-    }
 
     #endregion
 }
